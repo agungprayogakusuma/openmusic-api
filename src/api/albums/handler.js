4f62dacf2abd9e1d100d17843ead0bae23263759
Agung Prayoga Kusuma <agungpry00@gmail.com>
2023-12-18 20:32:14 +0700

    Menginisialisasi proyek OpenMusic V2

---
+++ src/api/albums/handler.js
@@ -0,0 +1,64 @@
+class AlbumsHandler {
+  constructor(albumsService, songsService, validator) {
+    this._albumsService = albumsService;
+    this._songsService = songsService;
+    this._validator = validator;
+  }
+
+  async postAlbumHandler(request, h) {
+    this._validator.validateAlbumPayload(request.payload);
+    const { name, year } = request.payload;
+
+    const albumId = await this._albumsService.addAlbum({ name, year });
+
+    const response = h.response({
+      status: 'success',
+      message: 'Album berhasil ditambahkan',
+      data: {
+        albumId,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+
+  async getAlbumByIdHandler(request, h) {
+    const { id } = request.params;
+
+    const album = await this._albumsService.getAlbumById(id);
+    album.songs = await this._songsService.getSongByAlbumId(id);
+    
+    return {
+      status: 'success',
+      data: {
+        album,
+      },
+    };
+  }
+
+  async putAlbumByIdHandler(request, h) {
+    this._validator.validateAlbumPayload(request.payload);
+    const { id } = request.params;
+
+    const album = await this._albumsService.editAlbumById(id, request.payload);
+    
+    return {
+      status: 'success',
+      message: 'Album berhasil diperbarui',
+      data: {
+        album,
+      },
+    };
+  }
+
+  async deleteAlbumByIdHandler(request, h) {
+    const { id } = request.params;
+    await this._albumsService.deleteAlbumById(id);
+    return {
+      status: 'success',
+      message: 'Album berhasil dihapus',
+    };
+  }
+}
+
+module.exports = AlbumsHandler;
---
+++ src/services/inMemory/MusicsService.js
@@ -0,0 +1,124 @@
+const { nanoid } = require('nanoid');
+const NotFoundError = require('../../exceptions/NotFoundError');
+
+class MusicsService {
+  constructor() {
+    this._albums = [];
+    this._songs = [];
+  }
+
+  addAlbum({ name, year }) {
+    const id = `album-${nanoid(16)}`;
+
+    const newAlbum = {
+      id, name, year, songs: [],
+    };
+    this._albums.push(newAlbum);
+
+    const isSuccess = this._albums.filter((album) => album.id === id).length > 0;
+    if (isSuccess) {
+      return id;
+    }
+  }
+
+  getAlbumById(id) {
+    const album = this._albums.filter((n) => n.id === id)[0];
+    if (!album) {
+      throw new NotFoundError('Album tidak ditemukan');
+    }
+
+    this._songs.forEach((song) => {
+      if (song.albumId === album.id) {
+        album.songs.push({
+          id: song.id, title: song.title, performer: song.performer,
+        });
+      }
+    });
+    return album;
+  }
+
+  editAlbumById(id, { name, year }) {
+    const index = this._albums.findIndex((album) => album.id === id);
+    if (index === -1) {
+      throw new NotFoundError('Gagal memperbarui album. Id tidak ditemukan');
+    }
+
+    this._albums[index] = {
+      ...this._albums[index],
+      name,
+      year,
+    };
+
+    return this._albums[index];
+  }
+
+  deleteAlbumById(id) {
+    const index = this._albums.findIndex((album) => album.id === id);
+    if (index === -1) {
+      throw new NotFoundError('Album gagal dihapus. Id tidak ditemukan');
+    }
+    this._albums.splice(index, 1);
+  }
+
+  // Song services
+
+  addSong({
+    title, year, genre, performer, duration, albumId,
+  }) {
+    const id = `song-${nanoid(16)}`;
+
+    const newSong = {
+      id, title, year, genre, performer, duration, albumId,
+    };
+    this._songs.push(newSong);
+
+    const isSuccess = this._songs.filter((song) => song.id === id).length > 0;
+    if (isSuccess) {
+      return id;
+    }
+  }
+
+  getSongs() {
+    const songs = this._songs.map((n) => ({ id: n.id, title: n.title, performer: n.performer }));
+    return songs;
+  }
+
+  getSongById(id) {
+    const song = this._songs.filter((n) => n.id === id)[0];
+    if (!song) {
+      throw new NotFoundError('Lagu tidak ditemukan');
+    }
+    return song;
+  }
+
+  editSongById(id, {
+    title, year, genre, performer, duration, albumId,
+  }) {
+    const index = this._songs.findIndex((song) => song.id === id);
+    if (index === -1) {
+      throw new NotFoundError('Gagal memperbarui Lagu. Id tidak ditemukan');
+    }
+
+    this._songs[index] = {
+      ...this._songs[index],
+      title,
+      year,
+      genre,
+      performer,
+      duration,
+      albumId,
+    };
+
+    return this._songs[index];
+  }
+
+  deleteSongById(id) {
+    const index = this._songs.findIndex((song) => song.id === id);
+    if (index === -1) {
+      throw new NotFoundError('Lagu gagal dihapus. Id tidak ditemukan');
+    }
+    this._songs.splice(index, 1);
+  }
+}
+
+module.exports = MusicsService;
