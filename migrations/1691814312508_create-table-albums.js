6d9707c8d2b48cef07d732cc79c45841f1227d66
Agung Prayoga Kusuma <agungpry00@gmail.com>
2023-12-20 22:33:01 +0700

    Membuat tabel playlists

--- migrations/1691814312508_create-table-albums.js
+++ migrations/1691814312508_create-table-albums.js
@@ -17,8 +17,15 @@ exports.up = pgm => {
       notNull: true,
     },
   });
+
+  pgm.addConstraint(
+      'songs',
+      'fk_songs.album_id',
+      'FOREIGN KEY(album_id) REFERENCES albums(id) ON DELETE CASCADE',
+  );
 };
 
 exports.down = pgm => {
+  pgm.dropConstraint('songs', 'fk_songs.album_id');
   pgm.dropTable('albums');
 };
